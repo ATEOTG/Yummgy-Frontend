a43d16496c0ea829b674c201ea5a397f9c726a79
ATEOTG <bryansantos9797@gmail.com>
2024-01-24 11:48:33 -0500

    added mobile responsiveness to login page

--- src/components/pages/user/Login.js
+++ src/components/pages/user/Login.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { Fragment, useRef, useState } from "react";
 import "./Login.css";
 import { useNavigate } from "react-router-dom";
 import YummgyApi from "../../../apis/YummgyApi";
@@ -50,67 +50,78 @@ function Login(props) {
   };
 
   return (
-    <div className="border border-2 border-black rounded p-3 login-cont">
-      <form onSubmit={loginSubmitHandler}>
-        <div className="d-flex gap-4 mb-3">
-          <label
-            htmlFor="username"
-            className="border border-2 border-black rounded login-label fs-4 text-center py-1"
-          >
-            Username
-          </label>
-          <input
-            className="form-control border border-2 border-black rounded"
-            type="text"
-            id="username"
-            name="username"
-            ref={usernameRef}
-            placeholder="Enter your Username..."
-            required
-          />
-        </div>
-        <div className="d-flex gap-4">
-          <label
-            htmlFor="password"
-            className="border border-2 border-black rounded login-label fs-4 text-center py-1"
-          >
-            Password
-          </label>
-          <input
-            className="form-control border border-2 border-black rounded"
-            type="password"
-            id="password"
-            name="password"
-            ref={passwordRef}
-            placeholder="Enter your Password..."
-            required
-          />
-        </div>
-        <div className="d-flex justify-content-center mt-4 w-90 m-auto">
-          <button
-            onClick={loginSubmitHandler}
-            className="border border-2 border-black rounded p-3 px-5 fs-5 login-btn"
-          >
-            Login
-          </button>
-        </div>
-      </form>
-      {loginSuccess && (
-        <div className="w-100 d-flex justify-content-center mt-4">
-          <div className="alert alert-success w-50 text-center" role="alert">
-            Login Successful!
+    <Fragment>
+      <div className="border border-2 border-black rounded p-3 login-cont">
+        <form onSubmit={loginSubmitHandler}>
+          <div className="d-flex gap-4 mb-3 login-text-cont">
+            <label
+              htmlFor="username"
+              className="border border-2 border-black rounded login-label fs-4 text-center py-1"
+            >
+              Username
+            </label>
+            <input
+              className="form-control border border-2 border-black rounded"
+              type="text"
+              id="username"
+              name="username"
+              ref={usernameRef}
+              placeholder="Enter your Username..."
+              required
+            />
           </div>
-        </div>
-      )}
+          <div className="d-flex gap-4 login-text-cont">
+            <label
+              htmlFor="password"
+              className="border border-2 border-black rounded login-label fs-4 text-center py-1"
+            >
+              Password
+            </label>
+            <input
+              className="form-control border border-2 border-black rounded"
+              type="password"
+              id="password"
+              name="password"
+              ref={passwordRef}
+              placeholder="Enter your Password..."
+              required
+            />
+          </div>
+          <div className="d-flex justify-content-center mt-4 w-90 m-auto">
+            <button
+              onClick={loginSubmitHandler}
+              className="border border-2 border-black rounded p-3 px-5 fs-5 login-btn"
+            >
+              Login
+            </button>
+          </div>
+        </form>
+        {loginSuccess && (
+          <div className="w-100 d-flex justify-content-center mt-4">
+            <div className="alert alert-success w-50 text-center" role="alert">
+              Login Successful!
+            </div>
+          </div>
+        )}
 
-      {loginFailure && (
-        <div className="w-100 d-flex justify-content-center mt-4">
-          <div className="alert alert-danger w-50 text-center" role="alert">
-            {failureMessage}
+        {loginFailure && (
+          <div className="w-100 d-flex justify-content-center mt-4">
+            <div className="alert alert-danger w-50 text-center" role="alert">
+              {failureMessage}
+            </div>
           </div>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+
+      <div className="d-flex justify-content-center mt-4 w-90 m-auto login-btn-mobile">
+        <button
+          onClick={loginSubmitHandler}
+          className="border border-2 border-black rounded p-3 px-5 fs-5 "
+        >
+          Login
+        </button>
+      </div>
+    </Fragment>
   );
 }
 
